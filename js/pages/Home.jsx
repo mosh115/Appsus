58b95f1985771d3437141852487acf43376d575d
Moshe <moshrozner@gmail.com>
2022-01-02 00:20:40 +0200

    add home and about pages

---
+++ js/pages/Home.jsx
@@ -0,0 +1,11 @@
+export function Home() {
+    return (
+
+        <section className="home">
+            <h2 >Welcome to Appsus!</h2>
+            <p className="home-content">
+                Check your Email and keep your notes organized.
+            </p>
+        </section>
+    )
+}
